e27236e5b6120c8389e235e846028d1bfc62be76
Rishabh-offcial <rishabhbanchhor6267@gmail.com>
2025-11-13 23:56:38 +0530

    function starting

---
+++ sheryians coding school/Java Script learn evrything/function.js
@@ -0,0 +1,35 @@
+// this section is bit lengthly 
+
+// what is function & why function is used?
+
+// when we write any code they immediatly runs. but when we have to use taht line of code when wee click or make to use that code. then that time functtion is came to us and provide the ability to use that code after a specific action.
+
+// why
+// when we use function the Code is optimized because the perticular code is not repeat in the over all code . we just call a function ko run that code . (because of it the code become optimized)
+
+
+// function declaration rules:--
+// the name of function can not started from 
+//   1.  by a number 
+//  2.  by special character except _ and $
+//  3.  by space
+//  4.  by reserved keywords like var , let , const , function , return etc
+//  5.  function name should be meaningful
+//  6.  function name should be in camelCase
+//  7.  function name is case sensitive
+//  8.  function name can be of any length
+//  9.  function name can be alphanumeric
+//  10. function name should not be same as variable name in the same scope
+//  11. function name should not be same as another function name in the same scope
+//  12. function name should not be same as built-in function name in the same scope
+//  13. function name should not be same as object name in the same scope
+//  14. function name should not be same as array name in the same scope
+
+
+// 1. function declaration 
+
+function abcd(/*parameters if any*/){
+    //code to be executed
+}
+abcd(/* arguments */); // function call
+
